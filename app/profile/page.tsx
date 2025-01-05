37a0c7db8ed5013e7924b68bb9233b5fa1917b58
Lortas102066 <mitsukimorinaga@gmail.com>
2025-01-05 12:22:55 +1100

    Fixed the unused issue

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import Image from 'next/image'
 import TechStack from '../components/techStack'
 import { Button } from "@/components/ui/button"
